925f64b006b1c8925b24220380bdf023bdee7190
shiva-pasad <Shivaprasadgunaganti@gmail.com>
2024-06-12 18:28:27 +0530

    changes

---
+++ src/components/Table/Table.js
@@ -0,0 +1,34 @@
+export const Heading1=()=>{
+    return <table>
+        <tr>
+            <th>S.No</th>
+            <th>Name</th>
+            <th>Batch_Number</th>
+            <th>Place</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Ram_Prasad</td>
+            <td>5R</td>
+            <td>Kadapa</td>
+        </tr>
+        <tr>
+             <td>2</td>
+            <td>Raghu</td>
+            <td>5R</td>
+            <td>Bhodhan</td>
+        </tr>
+        <tr>
+             <td>3</td>
+             <td>Manikanta</td>
+             <td>5R</td>
+             <td>Vizag</td>
+        </tr>
+        <tr>
+             <td>4</td>
+             <td>Tharun</td>
+             <td>5R</td>
+             <td>HYD</td>
+        </tr>
+    </table>
+}
