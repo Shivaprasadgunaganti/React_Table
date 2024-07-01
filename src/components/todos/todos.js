676b2ed08ce14a2526359eb77814710561a710fc
shiva-pasad <Shivaprasadgunaganti@gmail.com>
2024-07-01 22:11:12 +0530

    d

--- src/components/todos/todos.js
+++ src/components/todos/todos.js
@@ -1,3 +1,61 @@
+
+
+import { Component } from "react";
+import ButtonComponent1 from "../Table/buttonComponet/buttonComponent";
+class TodoComponent extends Component {
+  state = {
+    todo: ["wake up at 7:00 am", "class at 9:00am"],
+  };
+  todoHandler = () => {
+    this.setState({
+      todo: [...this.state.todo, "dinner at 8pm"],
+    });
+  };
+
+  todoUpdateHandler = (index) =>{
+    
+    let newTodo=this.state.todo
+    newTodo[index]='wake up at 8:00am'
+
+    this.setState({
+      todo:newTodo
+    })
+  }
+  todoDeleteHandler = (index) => {
+    let newTodo=this.state.todo.filter((eachtodo,i) =>(index !== i))
+    
+    this.setState({
+      todo:newTodo
+    })
+  };
+  render() {
+    return (
+      <>
+        <ol>
+          {this.state.todo.map((eachtodo, index) => {
+            return (
+              <>
+                <li>{eachtodo}</li>
+                <ButtonComponent1
+                  text={"update"}
+                  onPress={() => this.todoUpdateHandler(index)}
+                />
+                <ButtonComponent1
+                  text={"delete"}
+                  onPress={() => this.todoDeleteHandler(index)}
+                />
+              </>
+            );
+          })}
+        </ol>
+        <ButtonComponent1 height={50} width={50} onPress={this.todoHandler} text={'add todo'} />
+      </>
+    );
+  }
+}
+
+export default TodoComponent;
+
 // import { Component } from "react"
 
 // class TodoComponent extends Component {
@@ -19,27 +77,40 @@
 // }
 
 // export default TodoComponent  
-import ButtonComponent1 from "../Table/buttonComponet/buttonComponent";
+// import ButtonComponent1 from "../Table/buttonComponet/buttonComponent";
 
-const { Component } = require("react");
+// const { Component } = require("react");
 
 
-class Todocompoment extends Component {
-    state={
-        todos:['wake up at 7:00 am','class at 9:00 am']
-    }
-    render() {
-        return (
-            <>
-            {
+// class Todocompoment extends Component {
+//     state={
+//         todos:['wake up at 7:00 am','class at 9:00 am']
+//     }
+//     render() {
+//         return (
+//             <>
+//             {
             
-                this.state.todos.map((eachtodo)=>{
-                   return <h2>{eachtodo}</h2>
-                })};
-               <ButtonComponent1 text={'add todo'}/>
-            </>
-        )
-    }
 
-}
-export default Todocompoment
+//                 this.state.todos.map((eachtodo)=>{
+//                    return <h2>{eachtodo}</h2>
+//                 })};
+//                <ButtonComponent1 text={'add todo'}/>
+//             </>
+//         )
+//     }
+
+// }
+// export default Todocompoment
+
+//                 this.state.todos.map((eachtodo)=>{
+//                    return <h2>{eachtodo}</h2>
+//                 })};
+//                <ButtonComponent1 text={'add todo'}/>
+//             </>
+//         )
+//     }
+
+// }
+// export default Todocompoment
+
