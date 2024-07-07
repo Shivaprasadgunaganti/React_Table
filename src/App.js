e10ae7510b463f40acff6267f9d83aae4ef55f5d
Shivaprasadgunaganti <shivaprasad06559@gmail.com>
2024-07-07 11:31:45 +0530

    date

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
-
-// import ProductListing from "./components/productListing/productListing";
-// import UseEffectEx3 from "./components/useEffect/useEffectExample3";
-import Greetings from "./Greetings/Greetings";
-// import RecipeDataTask from "./recipeDataTask/recipeDataTask";
-// import TableData from "./recipeDataTask/tableData";
-
-import RecipeDataTask from "./recipeDataTask/recipeDataTask";
-import TableData from "./recipeDataTask/tableData";
-
+import DateFunction, { DataFunction } from "./Data/Data";
 
 const App = () => {
-  const names = ["shiva", "gnana", "raghu", "sasi"];
   return (
-    // <div>
-    //   <Greetings name={names[0]}>How are shiva</Greetings>
-    //   <Greetings name={names[2]}>How are sasi</Greetings>
-    //   <Greetings name={names[2]}>How are gnana</Greetings>
-    // </div>
-    <div>
-
-      {names.map((eachdata, index) => {
-        return (
-          <Greetings name={eachdata}>
-            <h2>hello</h2>
-            <h3>weclome</h3>
-            <h3>hi</h3>
-            <h2>hyderabad</h2>
-            <h3>nalgonda</h3>
-            <h3>kphb</h3>
-          </Greetings>
-        
-          
-        )
-      })}
-
-      <TableData />
-      <RecipeDataTask />
-     
-
-    </div>
-  );
+   <div>
+     <DateFunction/>
+     <DataFunction/>
+   </div>
+  
+  )
 };
 export default App;
