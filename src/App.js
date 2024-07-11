a95eeefd088e413256eb82ee049909babd087455
Shivaprasadgunaganti <shivaprasad06559@gmail.com>
2024-07-11 17:51:35 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import Accordion1 from "./components/Accordian/Accordian";
 import  { ListItems1, ListItems2 } from "./components/lists/lists";
+import TodoComponent from "./components/todos/todos";
 
 
 const App = () => {
   return (
    <div>
    
-    <ListItems1/>
-    <ListItems2/>
+    {/* <ListItems1/>
+    <ListItems2/> */}
+    {/* <TodoComponent/> */}
+ 
     
    </div>
   
