0537e8b2d1a48e548c24a58f424c0b2a82d6f72b
Shivaprasadgunaganti <shivaprasad06559@gmail.com>
2024-07-11 17:19:07 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-<<<<<<< Updated upstream
+
 import  { ListItems1, ListItems2 } from "./components/lists/lists";
-=======
+
 
 import { UseReduceFun } from "./components/useReduce/useReduce";
 import UseReduceTodo from "./components/useReduce/useReduceTodo";
 import UseRef from "./components/useRef/useRef";
 
->>>>>>> Stashed changes
+
 
 
 const App = () => {
   return (
    <div>
    
-<<<<<<< Updated upstream
+
     <ListItems1/>
     <ListItems2/>
-=======
+
      {/* <UseReduceFun/> */}
      {/* <UseReduceTodo/> */}
      <UseRef/>
->>>>>>> Stashed changes
     
    </div>
   
